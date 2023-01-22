f27438fde85e90eff0a7d09ce6c4be7bf782f693
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-22 13:23:10 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,35 @@
+export const item = {
+    "id": "95d6caff-6634-4dda-a502-2eed8a830054",
+    "chats": [
+        {
+            "id": "ca9789ae-6fea-4cf6-a90c-99e6f433d85e",
+            "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png",
+            "user_name": "behind",
+            "last_message": "Process small child final. Data they benefit. Card a trade where draw marriage."
+        },
+        {
+            "id": "007e8e9d-7c65-4393-8a52-fed266b8c247",
+            "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/268.png",
+            "user_name": "white",
+            "last_message": "This director manage air factor thought care manager. Team about choose friend give director."
+        },
+        {
+            "id": "25e13d1b-7692-4038-9184-821cdafc871f",
+            "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/267.png",
+            "user_name": "either",
+            "last_message": "Recent thing must day. Happy impact thus around."
+        },
+        {
+            "id": "26854b56-4301-4165-ac38-490bddf9c365",
+            "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/300.png",
+            "user_name": "drive",
+            "last_message": "Less eat three avoid. Throughout newspaper trouble add maybe ever health animal. Improve change find but director. Health car open doctor."
+        },
+        {
+            "id": "7addff0c-c157-440b-a166-32369bff0357",
+            "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/62.png",
+            "user_name": "shake",
+            "last_message": "Thousand benefit read city. Just seek a sport identify best. Dream general military inside. Put wide dream think school."
+        }
+    ]
+}
